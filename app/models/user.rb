6dc920d53bb4ced686997890f47b11bb111715f3
Daniel Hampton <danielhampton@gmail.com>
2014-10-29 00:59:59 +0000

    Lab 12.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
-class User < ActiveRecord::Base
-  has_many :splatts
-  has_and_belongs_to_many :follows,
-      class_name: "User",
-      join_table: :follows,
-      foreign_key: :follower_id,
-      association_foreign_key: :followed_id
-  has_and_belongs_to_many :followers,
-      class_name: "User",
-      join_table: :follows,
-      foreign_key: :followed_id,
-      association_foreign_key: :follower_id
+class User < Hashie::Dash
+  property :email
+  property :name
+  property :password
+  property :blurb
+  property :follows
+  property :followers
 end
